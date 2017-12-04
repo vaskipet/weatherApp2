ecde0f4236a3add611b33a337d0c6e1f0a354874
vaskipet <pvaskin1@gmail.com>
2017-12-04 17:38:32 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// The data is hidden until something is written to the input or selected from the dropdown
+document.getElementById('rawdata').style.visibility = 'hidden';
+
+function getData(e) {
+    // after the function fires the data is visible for the user
+    document.getElementById('rawdata').style.visibility = 'visible';
+    // storing the location variable either from the input OR from the selection
+    var location = document.getElementById('location').value || document.getElementById('select').value;
+    // the url variable where the is the query address
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=71caaafea1cb132af3cecad39715f455';
+
+    // Creating an XMLHttpRequest object 
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.send();
+
+    // specifying what to do with the response 
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+
+            // parsing the data
+            jsonObj = JSON.parse(xhr.responseText);
+
+            var data = jsonObj;
+            // storing the temperature variables so i can use them later
+            var tempK = data.main.temp;
+            var tempC = (tempK - 273.15);
+
+            // displaying the data to the user
+            document.getElementById('cityname').innerHTML = '<h4 class="text-center mt-2">' + data.name + '</h4>';
+            document.getElementById('icon').innerHTML = '<img  class="rounded mx-auto d-block picture" src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">' + '<p class="text-center">' + (tempC).toFixed(0) + ' degrees celsius and ' + data.weather[0].description + '.</p>';
+        }
+    }
+}
+
+
+
+// just a small animation I did based on a tutorial nevermind about this
+function changeIcon() {
+    let faicon = document.getElementById('faicon');
+    faicon.innerHTML = '&#xf2dc;';
+
+    setTimeout(function() {
+        faicon.innerHTML = '&#xf185;';
+    }, 2000);
+    setTimeout(function() {
+        faicon.innerHTML = '&#xf0c2;';
+    }, 4000);
+    setTimeout(function() {
+        faicon.innerHTML = '&#xf0e7;';
+    }, 6000);
+    setTimeout(function() {
+        faicon.innerHTML = '&#xf2c7;';
+    }, 8000);
+}
+
+// run animation
+changeIcon();
+// run animation every 10 secs
+setInterval(changeIcon, 10000);
